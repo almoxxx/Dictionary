c9a36ef48d4c30f76e8d83564e84a026dc162ecc
almoxxx <120190420+almoxxx@users.noreply.github.com>
2023-03-24 16:13:25 +0100

    Fixing Meaning Function

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,17 +3,11 @@ import React from "react";
 export default function Meaning(props) {
   return (
     <div className="meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definition.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>{definition.definition}</p>
-            <p>
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
+      <h3>{props.definition.partOfSpeech}</h3>
+      <p>{props.definition.definition}</p>
+      <p>
+        <em>{props.definition.example}</em>
+      </p>
     </div>
   );
 }
