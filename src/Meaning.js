d02dd2c52cdc149fae523094c46a756427a7518f
almoxxx <120190420+almoxxx@users.noreply.github.com>
2023-04-01 10:46:01 +0200

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,18 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="meaning">
       <h3>{props.definition.partOfSpeech}</h3>
-      <p>{props.definition.definition}</p>
       <p>
-        <em>{props.definition.example}</em>
+        <strong>Definition: </strong>
+        {props.definition.definition}
       </p>
+      <p>
+        <strong>Example: </strong> <em>{props.definition.example}</em>
+      </p>
+      <Synonyms synonyms={props.definition.synonyms} />
     </div>
   );
 }
